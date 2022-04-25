52d648cc06ea921d0a3e4b66ee00e85a67caedfa
Shreyashpawar2 <97426822+Shreyashpawar2@users.noreply.github.com>
2022-04-25 12:35:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+
+     document.querySelector("#header").addEventListener("click",function(){
+         document.body.style.background="#fff5d6";
+         document.body.style.color="body";
+         document.querySelector("#footer").style.backgroundColor="#253943";
+          document.querySelector("#footer").style.color="white";
+        document.querySelector("#bl1").style.color="white";
+         document.querySelector("#bl1").style.backgroundColor="#253943";
+          document.querySelector("#bl2").style.color="white";
+         document.querySelector("#bl2").style.backgroundColor="#253943";
+         document.querySelector("#op").style.color="white";
+         document.querySelector("#op").style.backgroundColor="#253943";
+        document.querySelector("#footer").style.backgroundColor="#253943";
+          
+        
+           },)
+    
+    document.querySelector("#header1").addEventListener("click",function(){
+         document.body.style.background="#253943";
+           document.querySelector("#footer").style.backgroundColor="#96b7b5";
+         document.querySelector("#footer").style.color="white";
+             document.querySelector("#bl1").style.color="white";
+         document.querySelector("#bl1").style.backgroundColor="#E27D60";
+          document.querySelector("#bl2").style.color="whitw";
+         document.querySelector("#bl2").style.backgroundColor="#E27D60";
+         
+        
+        
+         document.querySelector(".navbar-custom").style.backgroundColor="#E27D60";
+           document.querySelector("#op").style.color="white";
+         document.querySelector("#op").style.backgroundColor="#E27D60";
+        document.querySelector("#footer").style.backgroundColor="#E27D60";
+        
+          
+         },)  
+     
+      document.querySelector("#h").addEventListener("click",function(){
+           document.querySelector("#dp").style.display="inline";
+         
+            
+         },)
+     document.querySelector("#h1").addEventListener("click",function(){
+           document.querySelector("#dp").style.display="none";
+         },)
+       document.querySelector("#hh").addEventListener("click",function(){
+           document.querySelector("#dp").style.display="inline";
+         
+            
+         },)
+     document.querySelector("#hh1").addEventListener("click",function(){
+           document.querySelector("#dp").style.display="none";
+         },)
+
+     
+            
+        
+        
+        
